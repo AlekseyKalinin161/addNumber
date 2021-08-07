650fcfc488da6908a758415f9b5eb5d4fbe3ebd6
AlekseyKalinin161 <Kalininaleksey61@gmail.com>
2021-08-07 18:56:12 +0300

    component add number to card

--- src/inputNumber.js
+++ src/inputNumber.js
@@ -1,22 +1,14 @@
-import React, {useState} from "react";
-import {Button} from "@material-ui/core";
+import React from "react";
 
 function InputNumber({inputValue, setInputValue}) {
-
-
 
     function getValue(event){
         setInputValue(event.target.value);
     }
 
-
-
     return (
         <div>
-            <form>
                 <input type='number' onChange={getValue} value={inputValue}/>
-
-            </form>
         </div>
     );
 }
